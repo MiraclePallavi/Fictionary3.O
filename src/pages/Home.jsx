70d6602dc629b9509acfea627a999f2bb36a8a2c
PallaviKumari <pallavi4757kumari@gmail.com>
2024-11-09 12:32:05 +0530

    add auth

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,21 @@
-// src/pages/Home.js
 import React from "react";
 import "./Home.css"; // for custom pixel and animation styles
-import cityscape from "/assets/cityscape.png"; // Import the image
-import Footer from "../components/Footer/Footer";
-import { GoogleLogin } from '@react-oauth/google';
-
+import cityscape from "/assets/cityscape.png";
+import { GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
 const Home = () => {
+  const handleLoginSuccess = (credentialResponse) => {
+   
+    console.log("Login Success:", credentialResponse);
+    const decoded = jwtDecode(credentialResponse?.credential);
+    console.log(decoded)
+    // Handle successful login here (e.g., send credentialResponse to backend)
+  };
+
+  const handleLoginFailure = () => {
+    console.log("Login Failed");
+  };
+
   return (
     <div
       className="bg-dark-blue min-h-screen flex flex-col relative"
@@ -14,31 +24,20 @@ const Home = () => {
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
-      
       }}
     >
-      {/* Main Content */}
       <div className="flex-grow flex flex-col items-center justify-center z-10">
-        {/* Title */}
         <h1 className="text-neon-pink text-8xl font-pixel flicker">FICTIONARY</h1>
 
-        {/* Sign Up Button */}
-        <button className="sign-up-button font-pixel text-2xl text-amber-300 mt-8">
-        <GoogleLogin
-  onSuccess={credentialResponse => {
-    console.log(credentialResponse);
-  }}
-  onError={() => {
-    console.log('Login Failed');
-  }}
-/>;
-        </button>
-
-        {/* Additional Effects */}
-        <div className="pixel-stars"></div>
+        {/* Google Login Button */}
+        <div className="mt-8">
+          <GoogleLogin
+            onSuccess={handleLoginSuccess}
+            
+            onError={handleLoginFailure}
+          />
+        </div>
       </div>
-
-     
     </div>
   );
 };

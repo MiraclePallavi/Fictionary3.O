b1d562fe7d2f2e0ce6d8f7759a683750cf3aecd4
PallaviKumari <pallavi4757kumari@gmail.com>
2024-12-24 00:17:26 +0530

    change confing

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
-    proxy: {
-      '/api' : 'http://localhost:5173'
-    },
-    build: {
-      outDir: 'dist',  // Vercel expects the build to be in the 'dist' folder
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
     },
   },
-  plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Vercel expects the build to be in the 'dist' folder
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+});

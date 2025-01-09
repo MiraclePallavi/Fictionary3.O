a7907e6b725e6887d72068564456f78fcdc4f860
PallaviKumari <pallavi4757kumari@gmail.com>
2025-01-09 18:12:20 +0530

    fix ui

--- src/pages/Play/HintModal.jsx
+++ src/pages/Play/HintModal.jsx
@@ -1,7 +1,7 @@
 import styles from "./HintModel.module.css";
 import { useState, useEffect } from "react";
 import endpoints from "../../utils/APIendpoints";
-import useContext from "../context/UserContext";
+import useContext from "../../utils/Context";
 
 export default function HintModal(props) {
   const [modalOpen, setmodalOpen] = useState(props.open);
@@ -15,66 +15,66 @@ export default function HintModal(props) {
         headers: {
           Authorization: `Token ${token}`,
         },
-      })
-        .then((res) => res.json())
-        .then((serverResponse) => {
+      }).then((res) => {
+        res.json().then((serverResponse) => {
           if (res.status === 200) {
             setLoaded(true);
             if (serverResponse.success) {
-              let clue = serverResponse.clue.replace(/\n/g, "<br />");
+              var clue = serverResponse.clue;
+              while (clue.indexOf("\n") > -1) {
+                clue = clue.replace("\n", "<br />");
+              }
               const start = clue.indexOf("__linkstart__");
               const end = clue.indexOf("__linkend__");
               if (start > -1 && end > -1) {
                 clue =
                   clue.slice(0, start) +
-                  `<a href="${clue.slice(
-                    start + 13,
-                    end
-                  )}" target="_blank" class="${styles.neonLink}">` +
+                  '<a href="' +
+                  clue.slice(start + 13, end) +
+                  '" target="blank">' +
                   clue.slice(start + 13, end) +
                   "</a>" +
                   clue.slice(end + 11);
               }
               setClue(clue);
             } else {
-              setClue(serverResponse.message);
+              setClue([serverResponse.message]);
             }
           }
         });
+      });
     };
-
     setmodalOpen(props.open);
     setLoaded(false);
     if (props.open) {
       getClue();
     }
   }, [props, token]);
-
   const handleClick = (evt) => {
-    if (evt.target.id !== "hintModalBox") {
+    if (evt.target !== document.getElementById("hintModalBox")) {
       setmodalOpen(false);
       props.onClose();
     }
   };
-
   return (
     <div
-      className={styles.hintModalOverlay}
-      style={{ display: modalOpen ? "flex" : "none" }}
+      className={styles.rules1}
+      style={{ display: modalOpen ? "block" : "none" }}
       onClick={handleClick}
     >
-      <div id="hintModalBox" className={styles.hintModalBox}>
-        {loaded ? (
-          <>
-            <h2 className={`${styles.title} ${styles.neonText}`}>Clue</h2>
-            <p
-              className={`${styles.clueText} ${styles.neonParagraph}`}
-              dangerouslySetInnerHTML={{ __html: clue }}
-            ></p>
-          </>
-        ) : (
-          <h2 className={`${styles.title} ${styles.neonText}`}>Loading...</h2>
-        )}
+      <div id="hintModalBox" className={styles.modal}>
+        {(() => {
+          if (loaded) {
+            return (
+              <>
+                <h2 className={styles.h2}>Clue</h2>
+                <p className={styles.p} dangerouslySetInnerHTML={{__html: clue}}></p>
+              </>
+            );
+          } else {
+            return <h2 className={styles.h2}>Loading...</h2>;
+          }
+        })()}
       </div>
     </div>
   );

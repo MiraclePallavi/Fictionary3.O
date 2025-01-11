2b6439e56a89dfb38e085e18c7cce96ded6fd32f
PallaviKumari <pallavi4757kumari@gmail.com>
2025-01-11 22:25:29 +0530

    add updates

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import PowerUpShop from "./pages/cards/PowerUpShop";
 import Question from "./pages/Play/Question";
 import Navbar from "./components/Navbar/Navbar";
 import GameFinished from "./pages/finish/game-finished";
+
 import Updates from "./pages/cards/Updates";
 const App = () => {
   return (

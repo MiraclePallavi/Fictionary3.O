4cc0fb6ec57c03b9e5b13484a8e99609e6a0f663
PallaviKumari <pallavi4757kumari@gmail.com>
2025-01-15 10:51:06 +0530

    add info

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -135,7 +135,7 @@ const Home = () => {
           left: 0,
           width: "100%",
           height: "100%",
-          backgroundColor: "rgba(0, 0, 0, 0.2)", 
+          backgroundColor: "rgba(0, 0, 0, 0.5)", 
           zIndex: 1,
         }}
       ></div>
